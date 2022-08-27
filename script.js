07def93adfcdf08030e15ded2d064ded583cc05b
gshrmafp <76568662+gshrmafp@users.noreply.github.com>
2022-08-27 10:51:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+const scriptURL = ('https://script.google.com/macros/s/AKfycbwfKE31dZYJeEizLtOeLJjwOeScrt9QRMDcRVPdTIAc_jhuyoY3OO5GCjyqv_TP1f9y/exec');
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+})
